18b53b99112722885c66162d3e9994901fe4421c
Caroline Souza <loh_jg@hotmail.com>
2021-01-04 21:37:18 -0300

    Entidades e relacionamentos

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -0,0 +1,126 @@
+package com.devsuperior.dsdeliver.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Order implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String address;
+	private double latitude;
+	private double longitude;
+	private Instant moment;//para trabalhar com tempo
+	private OrderStatus status;
+	private double total;
+	
+	//para representar a associação de um pedido tendo 1 ou varios produtos e criado uma coleção. 
+	//na regra de negocio não adimite um produto sendo repetido em um mesmo pedido, por isso usou-se o Set, por não aceitar repetição
+	
+	private Set<Product>products= new HashSet<>();
+	
+	public Order () {
+		
+	}
+	//NAO SE COLOCA COLEÇÕES EM CONSTRUTORES
+	public Order(Long id, String address, double latitude, double longitude, Instant moment, OrderStatus status,
+			double total) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+		this.total = total;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public Set<Product> getProducts() {
+		return products;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+}
