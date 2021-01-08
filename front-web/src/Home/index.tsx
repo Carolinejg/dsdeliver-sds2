c40847b631d3ae1a227746742dc9cd86907399ce
Caroline Souza <loh_jg@hotmail.com>
2021-01-08 01:58:44 -0300

    Adicionando cabeçalho

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,29 +1,33 @@
 import './styles.css';
 import {ReactComponent as MainImage} from './main.svg'
+import Footer from '../Footer';
 
 function Home(){//criando a estrutura do componente 
     return(
-        <div className="home-container">
-            <div className="home-content"/*colocando todo o conteudo da pagina em um container para centralizar*/>
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br/> que entregamos 
-                        <br/> pra você!!!
-                    </h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos 
-                        <br/> levaremoss na sua porta
-                    </h3>
-                    <a href="orders" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
+        <>
+            <div className="home-container">
+                <div className="home-content"/*colocando todo o conteudo da pagina em um container para centralizar*/>
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br/> que entregamos 
+                            <br/> pra você!!!
+                        </h1>
+                        <h3 className="home-subtitle">
+                            Escolha o seu pedido e em poucos minutos 
+                            <br/> levaremoss na sua porta
+                        </h3>
+                        <a href="orders" className="home-btn-order">
+                            FAZER PEDIDO
+                        </a>
+                    </div>
+                    <div className="home-image">
+                        <MainImage/>
+                    </div>
                 </div>
-                <div className="home-image">
-                    <MainImage/>
-                </div>
-            </div>
 
-        </div>
+            </div>
+            <Footer/>
+        </>
     )
 
 }
