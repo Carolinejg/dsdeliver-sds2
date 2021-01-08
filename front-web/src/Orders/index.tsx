2fbbb6986b7165403d0f8f8ad2a283b5502340ee
Caroline Souza <loh_jg@hotmail.com>
2021-01-08 02:55:13 -0300

    adicionando as rotas da pagina principal

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function Orders(){//criando a estrutura do componente 
+    return(
+        <div>
+            <h1>Orders</h1>
+        </div>
+    )
+
+}
+
+export default Orders;
