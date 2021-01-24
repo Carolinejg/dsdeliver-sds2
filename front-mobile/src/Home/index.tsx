6a3d48f462a1b2de44e6bbd3b29dd2c545889f16
Caroline Souza <loh_jg@hotmail.com>
2021-01-23 22:37:21 -0300

    Adicionando listagem de produto

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,23 +1,28 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header'
 
 function Home() {
-    const handleOnPress = () =>{
+    const navigation = useNavigation();
 
+    const handleOnPress = () => {
+        navigation.navigate('Orders');
     }
     return (
         <>
+            <Header />
             <View style={styles.container}>
-            <Image source={require('../assets/deliveryman.png')} />
-            <Text style={styles.title}>
-                Acompanhe os pedidos e {'\n'} entregue no prazo!
+                <Image source={require('../assets/deliveryman.png')} />
+                <Text style={styles.title}>
+                    Acompanhe os pedidos e {'\n'} entregue no prazo!
             </Text>
-            <Text style={styles.subTitle}>
-                Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
+                <Text style={styles.subTitle}>
+                    Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
             </Text>
             </View>
-            <View style={styles.footer}> 
+            <View style={styles.footer}>
                 <RectButton style={styles.button} onPress={handleOnPress}>
                     <Text style={styles.buttonText}>VER PEDIDOS!</Text>
                 </RectButton>
